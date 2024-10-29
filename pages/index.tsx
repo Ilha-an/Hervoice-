0a13161fb4a5fbd195868d876df1e7af535f6fb8
Ilha-an <ilhanbirik.bd@gmail.com>
2024-10-29 07:18:21 -0700

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+// pages/index.tsx
+
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1 style={{ fontSize: "2rem", fontWeight: "bold" }}>Welcome to My Website</h1>
+      <p style={{ fontSize: "1.2rem", marginTop: "1rem" }}>
+        This is the home page, built using Next.js. Feel free to explore the site and learn more about what we offer.
+      </p>
+      {/* Add additional sections or links to other pages here */}
+    </div>
+  );
+}
